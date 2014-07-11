e46e216ceb4168218a4103831203efc4e43eec73
Nick Sandberg <nss118@gmail.com>
2014-07-11 09:50:46 -0700

    vehicle full service history works

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,12 +1,15 @@
 module VehiclesHelper
 
-	def current_services(vehicle, services)
+	def current_services(vehicle, services, type)
 		current = []
     services.each do |service|
       current << vehicle.vehicle_services.where("service_type_id = #{service}").order(:updated_at).last
     end
-    render partial: 'current_services', locals: { services: current }
-    
+    render partial: 'services', locals: { services: current, type: type } 
+  end
+
+  def full_service_history(vehicle, type)
+    render partial: 'services', locals: { services: vehicle.vehicle_services.all.order('updated_at DESC'), type: type }
   end
     
 end
