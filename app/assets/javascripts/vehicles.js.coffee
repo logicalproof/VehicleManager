a9d72a6737c3f0d4611edeec0ed93dfac2d01bbf
Nick Sandberg <nss118@gmail.com>
2014-07-12 22:53:28 -0700

    adding jquery show hide for service history

--- app/assets/javascripts/vehicles.js.coffee
+++ app/assets/javascripts/vehicles.js.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+ready = ->
+$("#toggle").click ->
+  $("#service-history").show()
+  return
+$(document).ready(ready)
+$(document).on('page:load', ready)
--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -11,6 +11,11 @@ module VehiclesHelper
   def full_service_history(vehicle)
     render partial: 'services', locals: { services: vehicle.vehicle_services.all.order('updated_at DESC'), title: "Full Service History" }
   end
+
+  def show_service_buttons
+    service_types = ServiceType.all
+    render partial: 'service_buttons', locals: {service_types: service_types}
+  end
     
 end
 
