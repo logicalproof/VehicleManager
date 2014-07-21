8b78f3e9e3713749250b4da595204757c97e2b3a
Nick Sandberg <nss118@gmail.com>
2014-07-21 13:51:09 -0700

    started a user page to show their assigned vehicles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show]
+  before_action :authenticate_user!
+
 	def index
 		@users = User.all
 	end
+
+	def show
+    @vehicles = []
+    if @user.vehicle_assignments
+      @user.vehicle_assignments.each do |x|
+        @vehicles << Vehicle.find(x.vehicle.id)
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
 
   devise_for :users 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
   resources :service_types
 
   resources :vehicles
